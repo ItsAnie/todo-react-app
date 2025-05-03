61631bb5003d692fee8d900f1a8d38b5d68f14dc
GM Service <anisahakyan524@gmail.com>
2025-05-03 16:10:39 +0400

    Initial commit

---
+++ src/TodoItem.jsx
@@ -0,0 +1,23 @@
+import "./TodoItem.css"
+
+function TodoItem({todo, onChange, onDelete}){
+    return(
+        <div className="todoListItem">
+            <label>
+                <input type="checkbox" checked={todo.isCompleted} onChange={(e) =>{
+                    onChange({
+                        ...todo,
+                        isCompleted: e.target.checked
+                    });
+                }}/>
+                {todo.text}
+            </label>
+            <button
+                    className="todoListItemDelete" 
+                    onClick={() => {
+                    onDelete(todo);
+                }}>X</button>
+        </div>
+    )
+}
+export default TodoItem
